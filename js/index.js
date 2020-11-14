c3b56ef33091185c66aac46e063ea4ba7c896939
Lape15 <larape1993@gmail.com>
2020-11-14 17:46:55 +0100

    updates

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 import { REPO_INFO } from "./query";
 
 const token = process.env.API_KEY;
-let profile;
+let profile = null;
 let image = document.querySelector("img");
 const imgContainer = document.querySelector(".profile-img-con");
 const profileName = document.querySelector(".profile-name");
 let loading = false;
+const userName = document.querySelector(".user-name");
+const bio = document.querySelector(".bio");
 const URL = "https://api.github.com/graphql";
+const activeTab = document.querySelector(".active");
 
 const fetchRepositories = (url) => {
   return fetch(url, {
@@ -29,6 +32,18 @@ response
     loading = false;
     profile = response.data;
     console.log(profile);
-    image.src = profile.viewer.avatarUrl;
-    profileName.innerText = profile.viewer.name;
+
+    if (!loading && profile !== null) {
+      image.src = profile.viewer.avatarUrl;
+      profileName.innerText = profile.viewer.name;
+      userName.innerText = profile.viewer.login;
+      bio.innerText = profile.viewer.bio;
+      var repoCount = document.createElement("SPAN");
+      let count = profile.viewer.repositories.nodes.length;
+      repoCount.innerText = count;
+      repoCount.classList.add("count");
+      activeTab.appendChild(repoCount);
+      let repositories = profile.viewer.repositories.nodes;
+      console.log(repositories);
+    }
   });
